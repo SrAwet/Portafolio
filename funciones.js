7ee2cf5e19016e3c93470850899d666cc58b0287
Willian Alexander <willian.zape65@gmail.com>
2023-05-21 19:00:30 -0600

    Cambio Contacto y más arreglos

--- funciones.js
+++ funciones.js
@@ -46,3 +46,24 @@ function onIntersection(entries) {
     }
   });
 }
+
+
+//Función para el form de contact
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
